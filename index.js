4fb50390a0de9caf99fe76a64450b8c0560804bd
jerinjamandev <jerinjaman.dev@gmail.com>
2025-10-14 01:28:47 +0600

    add2

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -28,7 +29,6 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
   }
 }
 run().catch(console.dir);
