5b1034d8ffccb3711e1a060703e8781befaab8c5
jerinjamandev <jerinjaman.dev@gmail.com>
2025-10-15 14:26:47 +0600

    add5

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ async function run() {
      })
 
 
-     app.get('/foods/:id',async(req,res)=>{
+     app.get('/food/:id',async(req,res)=>{
       const id=req.params.id;
       const query={_id:new ObjectId(id)}
       const result=await foodscollection.findOne(query)
